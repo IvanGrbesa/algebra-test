2fcc1f9323f5b30593d8b6702e51f54b80ca04d0
IvanGrbesa <ivangrbesa89@gmail.com>
2021-01-27 18:54:07 +0100

    add new console log

--- src/data.js
+++ src/data.js
@@ -3,6 +3,8 @@ import jsonp from "jsonp";
 function fetchsong(term) {
   const songUrl = `https://itunes.apple.com/search?term=${term}&entity=song `;
   console.log("songUrl", songUrl);
+  console.log("Nova linija");
+
   return new Promise((resolve) => {
     jsonp(songUrl, (error, response) => {
       console.log("succes!", response.results);
